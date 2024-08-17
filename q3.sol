129243c702c2e8e0b356d9d94d1e9a9987895d61
JimmyPranami18 <navinkumarjimmy.22.cse@iite.indusuni.ac.in>
2024-08-17 21:06:12 +0530

    Create q3.sol

---
+++ q3.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Greeting {
+    string public greeting;
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return greeting;
+    }
+}
